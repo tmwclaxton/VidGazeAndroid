1f8b3c1f74eeefd8604850f5c370c318836093bb
Toby Claxton <57643714+tmwclaxton@users.noreply.github.com>
2022-04-13 20:03:58 +0100

    broken but we're making progress
    tried to get fragments working

--- app/src/main/java/com/example/vidgaze/MainActivity.java
+++ app/src/main/java/com/example/vidgaze/MainActivity.java
@@ -1,17 +1,61 @@
 package com.example.vidgaze;
 
+import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.myToolBar);
+        toolbar = findViewById(R.id.myToolBar);
         setSupportActionBar(toolbar);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        /*if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }*/
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener =
+            item -> {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_categories:
+                        selectedFragment = new CategoriesFragment();
+                        break;
+                    case R.id.nav_subscriptions:
+                        selectedFragment = new SubscriptionsFragment();
+                        break;
+                    case R.id.nav_library:
+                        selectedFragment = new LibraryFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            };
+
 }
