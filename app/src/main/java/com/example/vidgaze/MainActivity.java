4a5bf4c594d3b24ceb334c9d0649d7f9b9bdd5ed
Toby Claxton <57643714+tmwclaxton@users.noreply.github.com>
2022-04-17 16:50:53 +0100

    I have gotten fragments working

--- app/src/main/java/com/example/vidgaze/MainActivity.java
+++ app/src/main/java/com/example/vidgaze/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.vidgaze;
 
 import androidx.annotation.NonNull;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,52 +9,142 @@ import android.view.MenuItem;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.vidgaze.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
-    private BottomNavigationView bottomNavigationView;
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    CategoriesFragment categoriesFragment = new CategoriesFragment();
+    LibraryFragment libraryFragment = new LibraryFragment();
+    SubscriptionsFragment subscriptionsFragment = new SubscriptionsFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+      //  BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notification);
+     //   badgeDrawable.setVisible(true);
+      //  badgeDrawable.setNumber(8);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.nav_library:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,libraryFragment).commit();
+                        return true;
+                    case R.id.nav_subscriptions:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,subscriptionsFragment).commit();
+                        return true;
+                    case R.id.nav_categories:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,categoriesFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+}
+
+/*
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         toolbar = findViewById(R.id.myToolBar);
         setSupportActionBar(toolbar);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new CategoriesFragment());
 
-        //I added this if statement to keep the selected fragment when rotating the device
-        /*if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new HomeFragment()).commit();
-        }*/
-    }
 
-    private NavigationBarView.OnItemSelectedListener navListener =
-            item -> {
-                Fragment selectedFragment = null;
-
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
                 switch (item.getItemId()) {
                     case R.id.nav_home:
-                        selectedFragment = new HomeFragment();
-                        break;
-                    case R.id.nav_categories:
-                        selectedFragment = new CategoriesFragment();
-                        break;
-                    case R.id.nav_subscriptions:
-                        selectedFragment = new SubscriptionsFragment();
+                        fragment = new HomeFragment();
                         break;
                     case R.id.nav_library:
-                        selectedFragment = new LibraryFragment();
+                        fragment = new LibraryFragment();
+                        break;
+                    case R.id.nav_categories:
+                        fragment = new CategoriesFragment();
+                        break;
+                    case R.id.nav_subscriptions:
+                        fragment = new SubscriptionsFragment();
                         break;
                 }
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        selectedFragment).commit();
-
+                loadFragments(fragment);
                 return true;
-            };
+            }
+        });
+       // loadFragments(new HomeFragment());
+        /*
+            Fragment fragment = null;
+            switch () {
+                case R.id.nav_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.nav_library:
+                    fragment = new LibraryFragment();
+                    break;
+                case R.id.nav_categories:
+                    fragment = new CategoriesFragment();
+                    break;
+                case R.id.nav_subscriptions:
+                    fragment = new SubscriptionsFragment();
+                    break;
+            }
 
-}
+        loadFragments(fragment);*//*
+    }
+
+
+
+
+    private boolean loadFragments(Fragment fragment) {
+        if (fragment == null) {
+            return false;
+        } else {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container,fragment);
+            fragmentTransaction.commit();
+            return true;
+        }
+    }
+}*/
